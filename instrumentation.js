4d895945a34c196aae2fdbdb572f9fbf03b537cf
Meritj <tjampens.remi@gmail.com>
2025-11-16 22:22:22 +0100

    eee

--- instrumentation.js
+++ instrumentation.js
@@ -36,5 +36,5 @@ export function register() {
     traceExporter: exporter,
   });
   
-  console.log('✅ [INSTRUMENTATION] OpenTelemetry configuré');
+  console.log('✅ [INSTRUMENTATION] OpenTelemetry configuréé');
 }
