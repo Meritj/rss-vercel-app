5a0ee38a1f4988e379dd70f15bd4bd59b6e8465d
Meritj <tjampens.remi@gmail.com>
2025-11-17 21:54:51 +0100

    simplify code

--- instrumentation.js
+++ instrumentation.js
@@ -1,36 +1,22 @@
 import { registerOTel } from '@vercel/otel';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'; 
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 export function register() {
   
-  let exporter;
+  const exporter = new OTLPTraceExporter({
+    url: 'https://http-intake.logs.datadoghq.com/api/v2/otel/v1/traces',
+    headers: {
+      'DD-API-KEY': process.env.DD_API_KEY,
+    },
+  });
 
-  if (process.env.NODE_ENV === 'production' && process.env.DD_API_KEY) {
-    exporter = new OTLPTraceExporter({
-      url: 'https://otlp.datadoghq.com/v1/traces',
-      headers: {
-        'dd-api-key': process.env.DD_API_KEY,
-      },
-    });
-    console.log('[OTEL] Exportateur Datadog configuré.');
-
-  } else {
-    exporter = new ConsoleSpanExporter();
-    console.log('📝 [OTEL] Exportateur CONSOLE utilisé (Mode DEBUG ou DD_API_KEY manquante).');
-
-    if (!process.env.DD_API_KEY) {
-        console.error('DD_API_KEY manquante. Utilisation de ConsoleSpanExporter.');
-    }
-  }
-  
   registerOTel({
     serviceName: 'rss-vercel-app',
     traceExporter: exporter,
   });
   
-  console.log('✅ [INSTRUMENTATION] OpenTelemetry configuré');
+  console.log('✅ OpenTelemetry configuré avec exporteur Datadog');
 }
