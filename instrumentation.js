b75572fd1a406a649ae8784282f696b23cf71477
Meritj <tjampens.remi@gmail.com>
2025-11-17 22:16:24 +0100

    add metadatas

--- instrumentation.js
+++ instrumentation.js
@@ -10,12 +10,17 @@ export function register() {
     url: 'https://otlp.datadoghq.com/v1/traces',
     headers: {
       'DD-API-KEY': process.env.DD_API_KEY,
+      'Content-Type': 'application/x-protobuf',
     },
   });
 
   registerOTel({
     serviceName: 'rss-vercel-app',
     traceExporter: exporter,
+    resource:{
+      'service.name': 'rss-vercel-app',
+      'deployment.environment': process.env.VERCEL_ENV || 'production',
+    }
   });
   
   console.log('✅ OpenTelemetry configuré avec exporteur Datadog');
