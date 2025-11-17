721bdc06af67c666db0bee4a646bfacd4f4e0b06
Meritj <tjampens.remi@gmail.com>
2025-11-17 14:33:08 +0100

    try fix nextconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    instrumentationHook: true,
-  },
-}
+    // ... autres configurations si vous en avez ...
 
-module.exports = nextConfig
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+
+            config.externals.push(
+                'stream',
+                'fs',
+                'net',
+                'tls',
+                'zlib',
+                '@grpc/grpc-js' 
+            );
+        }
+        
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+                net: false,
+                tls: false,
+                stream: false,
+                zlib: false,
+            };
+        }
+
+        return config;
+    },
+};
+
+module.exports = nextConfig;
